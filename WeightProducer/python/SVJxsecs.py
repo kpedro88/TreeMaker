33f5c08e23f93d34074fd55b61d93ec5619834c0
Kevin Pedro <kpedro88@gmail.com>
2022-12-24 13:59:53 -0600

    update cross sections, naming, remove old samples

--- WeightProducer/python/SVJxsecs.py
+++ WeightProducer/python/SVJxsecs.py
@@ -4,147 +4,53 @@ from TreeMaker.WeightProducer.MCSampleValues import MCSampleValuesHelper
 XSValues = MCSampleValuesHelper.XSValues
 
 SVJxsecs = {
-    "mZprime-500"  : {
-        "CrossSection" : XSValues(XS_13TeV=71.37),
-    },
-    "mZprime-600"  : {
-        "CrossSection" : XSValues(XS_13TeV=33.22),
-    },
-    "mZprime-700"  : {
-        "CrossSection" : XSValues(XS_13TeV=18.17),
-    },
-    "mZprime-800"  : {
-        "CrossSection" : XSValues(XS_13TeV=10.75),
-    },
-    "mZprime-900"  : {
-        "CrossSection" : XSValues(XS_13TeV=7.04),
-    },
-    "mZprime-1000" : {
-        "CrossSection" : XSValues(XS_13TeV=4.612),
-    },
-    "mZprime-1100" : {
-        "CrossSection" : XSValues(XS_13TeV=3.021),
-    },
-    "mZprime-1200" : {
-        "CrossSection" : XSValues(XS_13TeV=2.039),
-    },
-    "mZprime-1300" : {
-        "CrossSection" : XSValues(XS_13TeV=1.459),
-    },
-    "mZprime-1400" : {
-        "CrossSection" : XSValues(XS_13TeV=1.057),
-    },
-    "mZprime-1500" : {
-        "CrossSection" : XSValues(XS_13TeV=0.77),
-    },
-    "mZprime-1600" : {
-        "CrossSection" : XSValues(XS_13TeV=0.5667),
-    },
-    "mZprime-1700" : {
-        "CrossSection" : XSValues(XS_13TeV=0.4327),
-    },
-    "mZprime-1800" : {
-        "CrossSection" : XSValues(XS_13TeV=0.3304),
-    },
-    "mZprime-1900" : {
-        "CrossSection" : XSValues(XS_13TeV=0.2479),
-    },
-    "mZprime-2000" : {
-        "CrossSection" : XSValues(XS_13TeV=0.1849),
-    },
-    "mZprime-2100" : {
-        "CrossSection" : XSValues(XS_13TeV=0.1384),
-    },
-    "mZprime-2200" : {
-        "CrossSection" : XSValues(XS_13TeV=0.1044),
-    },
-    "mZprime-2300" : {
-        "CrossSection" : XSValues(XS_13TeV=0.08124),
-    },
-    "mZprime-2400" : {
-        "CrossSection" : XSValues(XS_13TeV=0.06361),
-    },
-    "mZprime-2500" : {
-        "CrossSection" : XSValues(XS_13TeV=0.04977),
-    },
-    "mZprime-2600" : {
-        "CrossSection" : XSValues(XS_13TeV=0.03891),
-    },
-    "mZprime-2700" : {
-        "CrossSection" : XSValues(XS_13TeV=0.03042),
-    },
-    "mZprime-2800" : {
-        "CrossSection" : XSValues(XS_13TeV=0.02447),
-    },
-    "mZprime-2900" : {
-        "CrossSection" : XSValues(XS_13TeV=0.01969),
-    },
-    "mZprime-3000" : {
-        "CrossSection" : XSValues(XS_13TeV=0.0155),
-    },
-    "mZprime-3100" : {
-        "CrossSection" : XSValues(XS_13TeV=0.01209),
-    },
-    "mZprime-3200" : {
-        "CrossSection" : XSValues(XS_13TeV=0.009518),
-    },
-    "mZprime-3300" : {
-        "CrossSection" : XSValues(XS_13TeV=0.007494),
-    },
-    "mZprime-3400" : {
-        "CrossSection" : XSValues(XS_13TeV=0.006148),
-    },
-    "mZprime-3500" : {
-        "CrossSection" : XSValues(XS_13TeV=0.005036),
-    },
-    "mZprime-3600" : {
-        "CrossSection" : XSValues(XS_13TeV=0.004047),
-    },
-    "mZprime-3700" : {
-        "CrossSection" : XSValues(XS_13TeV=0.003252),
-    },
-    "mZprime-3800" : {
-        "CrossSection" : XSValues(XS_13TeV=0.002613),
-    },
-    "mZprime-3900" : {
-        "CrossSection" : XSValues(XS_13TeV=0.0021),
-    },
-    "mZprime-4000" : {
-        "CrossSection" : XSValues(XS_13TeV=0.001688),
-    },
-    "mZprime-4100" : {
-        "CrossSection" : XSValues(XS_13TeV=0.001356),
-    },
-    "mZprime-4200" : {
-        "CrossSection" : XSValues(XS_13TeV=0.00109),
-    },
-    "mZprime-4300" : {
-        "CrossSection" : XSValues(XS_13TeV=0.0008757),
-    },
-    "mZprime-4400" : {
-        "CrossSection" : XSValues(XS_13TeV=0.0007037),
-    },
-    "mZprime-4500" : {
-        "CrossSection" : XSValues(XS_13TeV=0.0005655),
-    },
-    "mZprime-4600" : {
-        "CrossSection" : XSValues(XS_13TeV=0.0004544),
-    },
-    "mZprime-4700" : {
-        "CrossSection" : XSValues(XS_13TeV=0.0003652),
-    },
-    "mZprime-4800" : {
-        "CrossSection" : XSValues(XS_13TeV=0.0002934),
-    },
-    "mZprime-4900" : {
-        "CrossSection" : XSValues(XS_13TeV=0.0002358),
-    },
-    "mZprime-5000" : {
-        "CrossSection" : XSValues(XS_13TeV=0.0001895),
-    },
-    "mZprime-5100" : {
-        "CrossSection" : XSValues(XS_13TeV=0.0001523),
-    },
+    "s-channel_mMed-500"  : { "CrossSection" : XSValues(XS_13TeV=79.21), },
+    "s-channel_mMed-600"  : { "CrossSection" : XSValues(XS_13TeV=53.32), },
+    "s-channel_mMed-700"  : { "CrossSection" : XSValues(XS_13TeV=35.89), },
+    "s-channel_mMed-800"  : { "CrossSection" : XSValues(XS_13TeV=24.16), },
+    "s-channel_mMed-900"  : { "CrossSection" : XSValues(XS_13TeV=16.26), },
+    "s-channel_mMed-1000" : { "CrossSection" : XSValues(XS_13TeV=10.95), },
+    "s-channel_mMed-1100" : { "CrossSection" : XSValues(XS_13TeV=7.368), },
+    "s-channel_mMed-1200" : { "CrossSection" : XSValues(XS_13TeV=5.086), },
+    "s-channel_mMed-1300" : { "CrossSection" : XSValues(XS_13TeV=3.586), },
+    "s-channel_mMed-1400" : { "CrossSection" : XSValues(XS_13TeV=2.574), },
+    "s-channel_mMed-1500" : { "CrossSection" : XSValues(XS_13TeV=1.875), },
+    "s-channel_mMed-1600" : { "CrossSection" : XSValues(XS_13TeV=1.384), },
+    "s-channel_mMed-1700" : { "CrossSection" : XSValues(XS_13TeV=1.033), },
+    "s-channel_mMed-1800" : { "CrossSection" : XSValues(XS_13TeV=0.7789), },
+    "s-channel_mMed-1900" : { "CrossSection" : XSValues(XS_13TeV=0.5923), },
+    "s-channel_mMed-2000" : { "CrossSection" : XSValues(XS_13TeV=0.4537), },
+    "s-channel_mMed-2100" : { "CrossSection" : XSValues(XS_13TeV=0.3498), },
+    "s-channel_mMed-2200" : { "CrossSection" : XSValues(XS_13TeV=0.2713), },
+    "s-channel_mMed-2300" : { "CrossSection" : XSValues(XS_13TeV=0.2116), },
+    "s-channel_mMed-2400" : { "CrossSection" : XSValues(XS_13TeV=0.1658), },
+    "s-channel_mMed-2500" : { "CrossSection" : XSValues(XS_13TeV=0.1304), },
+    "s-channel_mMed-2600" : { "CrossSection" : XSValues(XS_13TeV=0.1029), },
+    "s-channel_mMed-2700" : { "CrossSection" : XSValues(XS_13TeV=0.08149), },
+    "s-channel_mMed-2800" : { "CrossSection" : XSValues(XS_13TeV=0.06476), },
+    "s-channel_mMed-2900" : { "CrossSection" : XSValues(XS_13TeV=0.0516), },
+    "s-channel_mMed-3000" : { "CrossSection" : XSValues(XS_13TeV=0.0412), },
+    "s-channel_mMed-3100" : { "CrossSection" : XSValues(XS_13TeV=0.03297), },
+    "s-channel_mMed-3200" : { "CrossSection" : XSValues(XS_13TeV=0.02644), },
+    "s-channel_mMed-3300" : { "CrossSection" : XSValues(XS_13TeV=0.02123), },
+    "s-channel_mMed-3400" : { "CrossSection" : XSValues(XS_13TeV=0.01707), },
+    "s-channel_mMed-3500" : { "CrossSection" : XSValues(XS_13TeV=0.01376), },
+    "s-channel_mMed-3600" : { "CrossSection" : XSValues(XS_13TeV=0.01109), },
+    "s-channel_mMed-3700" : { "CrossSection" : XSValues(XS_13TeV=0.008951), },
+    "s-channel_mMed-3800" : { "CrossSection" : XSValues(XS_13TeV=0.007234), },
+    "s-channel_mMed-3900" : { "CrossSection" : XSValues(XS_13TeV=0.005849), },
+    "s-channel_mMed-4000" : { "CrossSection" : XSValues(XS_13TeV=0.004731), },
+    "s-channel_mMed-4100" : { "CrossSection" : XSValues(XS_13TeV=0.003828), },
+    "s-channel_mMed-4200" : { "CrossSection" : XSValues(XS_13TeV=0.003099), },
+    "s-channel_mMed-4300" : { "CrossSection" : XSValues(XS_13TeV=0.002509), },
+    "s-channel_mMed-4400" : { "CrossSection" : XSValues(XS_13TeV=0.002032), },
+    "s-channel_mMed-4500" : { "CrossSection" : XSValues(XS_13TeV=0.001645), },
+    "s-channel_mMed-4600" : { "CrossSection" : XSValues(XS_13TeV=0.001331), },
+    "s-channel_mMed-4700" : { "CrossSection" : XSValues(XS_13TeV=0.001078), },
+    "s-channel_mMed-4800" : { "CrossSection" : XSValues(XS_13TeV=0.0008709), },
+    "s-channel_mMed-4900" : { "CrossSection" : XSValues(XS_13TeV=0.0007043), },
+    "s-channel_mMed-5000" : { "CrossSection" : XSValues(XS_13TeV=0.000569), },
+    "s-channel_mMed-5100" : { "CrossSection" : XSValues(XS_13TeV=0.0004593), },
     # Full t-channel cross section
     # currently commented out in favor of pair production cross sections (below)
     # todo for UL: improve naming scheme to disambiguate, recalculate cross sections w/ newer PDFs
