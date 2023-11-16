cbfddd8b48e406b58bdbd7f2ab69691205cdc318
Kevin Pedro <kpedro88@gmail.com>
2023-11-16 14:50:56 -0600

    2016 samples

---
+++ Production/test/condorSub/dict_Private1DUL16_svj.py
@@ -0,0 +1,7 @@
+flist = {
+    "scenario":  "Summer20UL16sig",
+    "args": "deepAK8=0 deepDoubleB=0 boostedsemivisible=1",
+    "samples": [
+        ["Private1DUL16.SVJ_UL2016_s-channel_mMed-350_mDark-10_rinv-0p3_alpha-peak_MADPT300_13TeV-madgraphMLM-pythia8_n-1000"],
+    ]
+}
