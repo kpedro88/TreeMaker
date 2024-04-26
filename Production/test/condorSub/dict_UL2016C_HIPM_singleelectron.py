d2197e8c271db0043d917b56aafad136ebb79e77
Kevin Pedro <kpedro88@gmail.com>
2024-04-26 08:32:29 -0500

    add branches to single electron

--- Production/test/condorSub/dict_UL2016C_HIPM_singleelectron.py
+++ Production/test/condorSub/dict_UL2016C_HIPM_singleelectron.py
@@ -1,5 +1,6 @@
 flist = {
     "scenario": "Summer20UL16APV_DATA",
+    "args": "jetsconstituents=2 tchannel=True",
     "samples": [
         ["Run2016C-UL2016_HIPM-v2.SingleElectron"],
    ]
--- Production/test/condorSub/dict_UL2016H_singleelectron.py
+++ Production/test/condorSub/dict_UL2016H_singleelectron.py
@@ -1,5 +1,6 @@
 flist = {
     "scenario": "Summer20UL16_DATA",
+    "args": "jetsconstituents=2 tchannel=True",
     "samples": [
         ["Run2016H-UL2016-v2.SingleElectron"],
    ]
