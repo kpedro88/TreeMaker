0ce7dbd925584e698dce22861a9dac56978e280d
Kevin Pedro <kpedro88@gmail.com>
2024-05-03 15:08:04 -0500

    dictionaries for sets of boosted samples

---
+++ Production/test/condorSub/dict_Private2024_boostedsvj.py
@@ -0,0 +1,8 @@
+flist = {
+    "scenario":  "Summer20UL18sig",
+    "args": "boostedsemivisible=1",
+    "samples": [
+        ["Private2024.s-channel_mMed-350_mDark-10_rinv-0p3_alpha-peak_MADPT300_13TeV-madgraphMLM-pythia8"],
+        ["Private2024.s-channel_mMed-450_mDark-10_rinv-0p3_alpha-peak_MADPT300_13TeV-madgraphMLM-pythia8"],
+    ]
+}
