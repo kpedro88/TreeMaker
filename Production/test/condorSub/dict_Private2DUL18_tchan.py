d6d46249e605f3fc01df1d160437dd6df280e516
Kevin Pedro <kpedro88@gmail.com>
2024-10-10 23:18:00 -0500

    scan part0: dicts

---
+++ Production/test/condorSub/dict_Private2DUL18_tchan.py
@@ -0,0 +1,18 @@
+flist = {
+    "scenario":  "Summer20UL18sig",
+    "args": "tchannel=1 jetsconstituents=2",
+    "samples": [
+        ["Private2DUL18.SVJ_t-channel_mMed-1000_mDark-20_rinv-0p3_alpha-peak_yukawa-1_13TeV-madgraphMLM-pythia8"],
+        ["Private2DUL18.SVJ_t-channel_mMed-1500_mDark-20_rinv-0p3_alpha-peak_yukawa-1_13TeV-madgraphMLM-pythia8"],
+        ["Private2DUL18.SVJ_t-channel_mMed-2000_mDark-20_rinv-0p3_alpha-peak_yukawa-1_13TeV-madgraphMLM-pythia8"],
+        ["Private2DUL18.SVJ_t-channel_mMed-2500_mDark-20_rinv-0p3_alpha-peak_yukawa-1_13TeV-madgraphMLM-pythia8"],
+        ["Private2DUL18.SVJ_t-channel_mMed-3000_mDark-20_rinv-0p3_alpha-peak_yukawa-1_13TeV-madgraphMLM-pythia8"],
+        ["Private2DUL18.SVJ_t-channel_mMed-3500_mDark-20_rinv-0p3_alpha-peak_yukawa-1_13TeV-madgraphMLM-pythia8"],
+        ["Private2DUL18.SVJ_t-channel_mMed-4000_mDark-20_rinv-0p3_alpha-peak_yukawa-1_13TeV-madgraphMLM-pythia8"],
+        ["Private2DUL18.SVJ_t-channel_mMed-500_mDark-20_rinv-0p3_alpha-peak_yukawa-1_13TeV-madgraphMLM-pythia8"],
+        ["Private2DUL18.SVJ_t-channel_mMed-600_mDark-20_rinv-0p3_alpha-peak_yukawa-1_13TeV-madgraphMLM-pythia8"],
+        ["Private2DUL18.SVJ_t-channel_mMed-700_mDark-20_rinv-0p3_alpha-peak_yukawa-1_13TeV-madgraphMLM-pythia8"],
+        ["Private2DUL18.SVJ_t-channel_mMed-800_mDark-20_rinv-0p3_alpha-peak_yukawa-1_13TeV-madgraphMLM-pythia8"],
+        ["Private2DUL18.SVJ_t-channel_mMed-900_mDark-20_rinv-0p3_alpha-peak_yukawa-1_13TeV-madgraphMLM-pythia8"],
+    ]
+}
