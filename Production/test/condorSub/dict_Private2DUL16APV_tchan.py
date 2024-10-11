183120e9413ad39d1dfc49b1be138600499efef9
Kevin Pedro <kpedro88@gmail.com>
2024-10-10 23:18:00 -0500

    scan part0: dicts

---
+++ Production/test/condorSub/dict_Private2DUL16APV_tchan.py
@@ -0,0 +1,8 @@
+flist = {
+    "scenario":  "Summer20UL16APVsig",
+    "args": "tchannel=1 jetsconstituents=2",
+    "samples": [
+        ["Private2DUL16APV.SVJ_t-channel_mMed-2000_mDark-20_rinv-0p3_alpha-peak_yukawa-1_13TeV-madgraphMLM-pythia8"],
+        ["Private2DUL16APV.SVJ_t-channel_mMed-600_mDark-20_rinv-0p3_alpha-peak_yukawa-1_13TeV-madgraphMLM-pythia8"],
+    ]
+}
